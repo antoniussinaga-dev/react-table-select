3db4563a96ad6fddd4ea90bceeb2236e1794acca
antoniussinaga-dev <antoniussinaga.dev@gmail.com>
2023-06-05 02:22:37 +0700

    first

---
+++ components/index.ts
@@ -0,0 +1,18 @@
+interface Nama {
+    nama: string;
+    umur: number;
+}
+export type People = {
+    nama: string;
+    age: number;
+}
+
+/**
+ *
+ * @returns nama Kamu
+ */
+export function nama() {
+    return "Hello Motepak"
+}
+
+export default Nama
